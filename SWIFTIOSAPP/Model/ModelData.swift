232d86b37040f6cffd64c043ca580d0a1b12ec76
junjie-mini <jfjunjieli@gmail.com>
2023-09-26 19:55:08 +0200

    feat: tutoria3

--- SWIFTIOSAPP/Model/ModelData.swift
+++ SWIFTIOSAPP/Model/ModelData.swift
@@ -6,6 +6,12 @@
 //
 
 import Foundation
+import Combine
+
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+}
+
 
 var landmarks: [Landmark] = load("landmarkData.json")
 
