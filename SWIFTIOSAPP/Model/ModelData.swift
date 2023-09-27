c91d5abe17812188c73a819bce5efb9c5f4d5f5a
junjie-mini <jfjunjieli@gmail.com>
2023-09-28 00:23:36 +0200

    feat: animation

--- SWIFTIOSAPP/Model/ModelData.swift
+++ SWIFTIOSAPP/Model/ModelData.swift
@@ -8,20 +8,20 @@
 import Foundation
 import Combine
 
+
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 }
 
-
-var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
 
 
